3a94a94fcae8fd645724d5f99d3e4604859846f1
TruongNM <manhtruonghedspi@gmail.com>
2018-07-14 17:21:39 +0700

    Add login activity

--- app/src/main/java/com/example/truongnm/smack/MainActivity.kt
+++ app/src/main/java/com/example/truongnm/smack/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.truongnm.smack
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
@@ -34,6 +35,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun loginBtnNavClicked(view: View) {
+        val loginIntent = Intent(this, LoginActivity::class.java)
+        startActivity(loginIntent)
 
     }
 
