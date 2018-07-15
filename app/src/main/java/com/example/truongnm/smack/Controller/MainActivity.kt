daf8dd6bca137fab8df885a8b4f7de584dcb21d5
TruongNM <manhtruonghedspi@gmail.com>
2018-07-15 18:20:27 +0700

    socket on

--- app/src/main/java/com/example/truongnm/smack/Controller/MainActivity.kt
+++ app/src/main/java/com/example/truongnm/smack/Controller/MainActivity.kt
@@ -14,29 +14,46 @@ import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
+import com.example.truongnm.smack.Model.Channel
 import com.example.truongnm.smack.R
 import com.example.truongnm.smack.Services.AuthService
+import com.example.truongnm.smack.Services.MessageService
 import com.example.truongnm.smack.Services.UserDataService
 import com.example.truongnm.smack.Utilities.BROADCAST_USER_DATA_CHANGE
+import com.example.truongnm.smack.Utilities.SOCKET_URL
+import io.socket.client.IO
+import io.socket.emitter.Emitter
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 import kotlinx.android.synthetic.main.nav_header_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    val socket = IO.socket(SOCKET_URL)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+        socket.connect()
+        socket.on("channelCreated", onNewChannel)
 
         val toggle = ActionBarDrawerToggle(
                 this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
-        hideKeyboard()
+    }
 
+    override fun onResume() {
         LocalBroadcastManager.getInstance(this).registerReceiver(userDataChangeReciever,
                 IntentFilter(BROADCAST_USER_DATA_CHANGE))
+        super.onResume()
+    }
+
+    override fun onDestroy() {
+        socket.disconnect()
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(userDataChangeReciever)
+        super.onDestroy()
     }
 
     private val userDataChangeReciever = object : BroadcastReceiver() {
@@ -87,17 +104,29 @@ class MainActivity : AppCompatActivity() {
                         val channelDesc = descTextField.text.toString()
 
                         // Create channel with the channel name and description
-                        hideKeyboard()
+                        socket.emit("newChannel", channelName, channelDesc)
                     }
-                    .setNegativeButton("Cancel") { dialogInterface, i ->
-                        hideKeyboard()
-                    }
+                    .setNegativeButton("Cancel") { dialogInterface, i -> }
                     .show()
         }
     }
 
-    fun sendMsgBtnClicked(view: View) {
+    private val onNewChannel = Emitter.Listener { args ->
+        runOnUiThread {
+            val channelName = args[0] as String
+            val channelDescription = args[1] as String
+            val channelId = args[2] as String
 
+            val newChannel = Channel(channelName, channelDescription, channelId)
+            MessageService.channels.add(newChannel)
+            println(newChannel.name)
+            println(newChannel.description)
+            println(newChannel.id)
+        }
+    }
+
+    fun sendMsgBtnClicked(view: View) {
+        hideKeyboard()
     }
 
     fun hideKeyboard() {
---
+++ app/src/main/java/com/example/truongnm/smack/Services/MessageService.kt
@@ -0,0 +1,7 @@
+package com.example.truongnm.smack.Services
+
+import com.example.truongnm.smack.Model.Channel
+
+object MessageService {
+    val channels = ArrayList<Channel>()
+}
